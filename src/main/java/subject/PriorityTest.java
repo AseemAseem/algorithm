66ddb673245dfc69dc96cf1224b878e9a65a8caa
aseem.chen <55646jddk@>
2021-09-09 15:50:46 +0800

    获取数组前3大的元素，优先级队列实现

---
+++ src/main/java/subject/PriorityTest.java
@@ -0,0 +1,31 @@
+package subject;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * output top 3
+ */
+public class PriorityTest {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 5, 4, 2};
+        int top = 3;
+
+        test(top, arr);
+    }
+
+    public static void test(int top, int[] arr) {
+        Queue<Integer> queue = new PriorityQueue<Integer>((a, b) -> b - a);
+        for (int i = 0; i < arr.length; i++) {
+            queue.add(arr[i]);
+            while (queue.size() > top) {
+                queue.poll();
+            }
+        }
+        System.out.println();
+        while (queue.size() > 0) {
+            System.out.print(queue.poll() + " ");
+        }
+        System.out.println();
+    }
+}
